868d1faa7b5e1109aec5926af21fe7fb7f09475e
qp75 <73798136+qp75@users.noreply.github.com>
2022-06-07 17:17:37 +0200

    Update HolaMon.java

--- M5UF1LuisGarcia/src/main/java/HolaMon.java
+++ M5UF1LuisGarcia/src/main/java/HolaMon.java
@@ -3,7 +3,7 @@ public class HolaMon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola món");
+		System.out.println("Corregit");
 	}
 
 }
